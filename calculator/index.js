6e906be542281fb51eda40d4e2eaa86c5607c5de
Malik113322 <121788489+Malik113322@users.noreply.github.com>
2023-08-10 18:00:23 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,46 @@
+// const {hello, callName} = require("./script");
+
+// hello();
+// callName("john");
+
+// // ES6
+
+// import { func } from "./script.js";
+
+// func();
+
+// import func from "./script";
+// func("john");
+// const func = require("./script");
+
+// func();
+
+// working on terminal because server side 
+
+// import func from "./script.js";
+
+// func("okk");
+
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+// const path = require("path");
+
+
+// app.get("/", (req, res) =>{
+//     res.sendFile(path.join(__dirname + "/index.html"))
+// });
+
+// app.get("/about", (req, res) =>{
+//     res.sendFile(path.join(__dirname + "/about.html"));
+// });
+
+// app.get("/contact", (req, res) =>{
+//     res.sendFile(path.join(__dirname + "/contact.html"))
+// })
+
+// app.listen(port, () =>{
+//     console.log(`http://localhost:${port}`)
+// });
+
+
